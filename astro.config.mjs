b5edd6640773507321b6c49cddff7651e153a092
dampuzakura <168283013+dampuzakura@users.noreply.github.com>
2024-10-20 02:25:32 +0900

    ➕ Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     UnoCSS({
       injectReset: true,
     }),
+    svelte(),
   ],
 });
